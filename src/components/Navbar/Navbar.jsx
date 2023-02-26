8d45581d10c89cad9636ba663e6d10a035220831
Charity Makhanu <makhanuhcharity@gmail.com>
2023-02-26 12:48:34 +0300

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,28 +7,28 @@ import './Navbar.css';
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
     return (
-      <nav className="app__navbar">
-        <div className="app__navbar-logo">
-          <img src={images.gericht} alt="app__logo" />
+      <nav className="app_navbar">
+        <div className="app_navbar-logo">
+          <img src={images.gericht} alt="app_logo" />
         </div>
-        <ul className="app__navbar-links">
-          <li className="p__opensans"><a href="#home">Home</a></li>
-          <li className="p__opensans"><a href="#about">About</a></li>
-          <li className="p__opensans"><a href="#menu">Menu</a></li>
-          <li className="p__opensans"><a href="#awards">Awards</a></li>
-          <li className="p__opensans"><a href="#contact">Contact</a></li>
+        <ul className="app_navbar-links">
+          <li className="p_opensans"><a href="#home">Home</a></li>
+          <li className="p_opensans"><a href="#about">About</a></li>
+          <li className="p_opensans"><a href="#menu">Menu</a></li>
+          <li className="p_opensans"><a href="#awards">Awards</a></li>
+          <li className="p_opensans"><a href="#contact">Contact</a></li>
         </ul>
-        <div className="app__navbar-login">
-          <a href="#login" className="p__opensans">Log In / Registration</a>
+        <div className="app_navbar-login">
+          <a href="#login" className="p_opensans">Log In / Registration</a>
           <div />
-          <a href="/" className="p__opensans">Book Table</a>
+          <a href="/" className="p_opensans">Book Table</a>
         </div>
-        <div className="app__navbar-smallscreen">
+        <div className="app_navbar-smallscreen">
           <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
           {toggleMenu && (
-            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-              <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
-              <ul className="app__navbar-smallscreen_links">
+            <div className="app_navbar-smallscreen_overlay flex_center slide-bottom">
+              <MdOutlineRestaurantMenu fontSize={27} className="overlay_close" onClick={() => setToggleMenu(false)} />
+              <ul className="app_navbar-smallscreen_links">
                 <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
                 <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
                 <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
